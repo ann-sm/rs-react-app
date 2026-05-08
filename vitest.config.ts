6f99de1cedc427fdb444a4bd79f6855b0db244fb
ann-sm <annsmirnovva@gmail.com>
2026-05-08 09:39:42 +0300

    chore: set up vitest config and pre-push hook

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    coverage: {
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/**/*.d.ts'
+      ],
+      thresholds: {
+        "global": {
+          "statements": 80,
+          "branches": 50,
+          "functions": 50,
+          "lines": 50
+        }
+      }
+    },
+  },
+});
