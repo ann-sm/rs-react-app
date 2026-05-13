931703264e8e5204695375cc07b20a5b0731ff4b
ann-sm <annsmirnovva@gmail.com>
2026-05-13 21:58:57 +0300

    fix: move error boundary into Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/Header';
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary';
 
 function Layout() {
   return (
-    <div>
+    <ErrorBoundary>
       <Header />
       <Outlet />
-    </div>
+    </ErrorBoundary>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.tsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import About from './pages/About/About.tsx';
 import NotFound from './pages/NotFound/NotFound.tsx';
@@ -39,8 +38,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <RouterProvider router={router} />
   </StrictMode>
 );
